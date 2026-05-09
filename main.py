a1a9d022364ab6bb11ab4aa89a9bd4a9f82003f4
Keqing Li <keqingli1129@gmail.com>
2026-05-09 14:40:18 -0500

    Implement first_agent in main.py and add langgraph.json for agent configuration

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langchain.agents import create_agent   
+
+load_dotenv()
+first_agent=create_agent(
+        model="deepseek-chat",
+        tools=[],
+        middleware=[],
+        system_prompt="you are a good assistant"
+    )
+
 def main():
+    
     print("Hello from langchain-agents-bucket!")
+    
+    response=first_agent.invoke(
+        {"messages":[{"role":"user", "content":"who are you"}]}
+    )
+    print(response)
 
 
 if __name__ == "__main__":
